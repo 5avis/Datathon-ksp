f2f8f97968311e03ff2e1196596cb8ebbe28cffb
5avis <273328604+5avis@users.noreply.github.com>
2026-07-26 16:41:04 +0530

    changed the files to make the web fast

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,9 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
-module.exports = nextConfig
+  reactStrictMode: false,
+  swcMinify: true,
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'recharts', 'reactflow'],
+  },
+};
+module.exports = nextConfig;
